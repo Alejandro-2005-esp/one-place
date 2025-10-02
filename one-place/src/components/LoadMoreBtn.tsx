dbc3f5f2ffc5190c9daf9e8810121c3659f4f0d4
Alejandro Acosta <alejo.acosta.q@gmail.com>
2025-10-02 17:43:00 -0500

    file/feat: main page develop, componets file created, buttons created

---
+++ one-place/src/components/LoadMoreBtn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoadMoreBtn(){
+    return(
+        <div>
+            <button>LOAD MORE</button>
+        </div>
+    );
+
+}
+export default LoadMoreBtn;
---
+++ one-place/src/components/SellAllBtn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SeelAllBtn(){
+    return(
+        <div>
+            <button>SEE ALL</button>
+        </div>
+    );
+
+}
+export default SeelAllBtn;
