54d663b98789f98d10cfa8bd0d58b6a44a2a39aa
Alejandro Acosta <alejo.acosta.q@gmail.com>
2025-11-27 12:33:45 -0500

    feat: view all button eliminated

--- one-place/src/App.tsx
+++ one-place/src/App.tsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 import Mainpage from './pages/MainPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import CommentPage from './pages/CommentPage'
+import AddProduct from './pages/AgregarProducto'
+
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ one-place/src/components/Comment.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Comment() {
+  return (
+    <>
+    <p>hola</p>
+    </>
+            
+      
+  );
+}
+
+export default Comment;
